021a8aa4dab1c840509ffaf2c201accad0a7f11b
Cedric-Z <514074577@qq.com>
2017-11-14 16:48:07 -0500

    add api router

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+/**
+ * Created by zhang on 2017/11/14.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/login', function (req, res, next) {
+    res.json({})
+});
+router.get('/register', function (req, res, next) {
+    res.json({})
+});
+
+
+module.exports = router;
